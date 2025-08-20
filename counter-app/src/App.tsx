574c9daec04c1099734c61f73010c7b09a69d7e7
eeeeei2 <ss.626tkhs@gmail.com>
2025-08-20 21:45:50 +0900

    カウンターアプリの実装

---
+++ counter-app/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>{count}</h1>
+      <button onClick={handleClick}>+</button>
+    </div>
+  );
+}
+
+export default App;
